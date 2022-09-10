c6290ac37e977046496b8297a8399feb6440c267
Silvia Santana <73367719+oliversms@users.noreply.github.com>
2022-09-10 00:46:21 -0300

    Add files via upload

---
+++ DoWhile.java
@@ -0,0 +1,42 @@
+package familia58;
+
+import java.util.Scanner;
+
+public class DoWhile {
+
+	public static void main(String[] args) {
+		int numeros,contMult3=0,somaMult3=0;
+		float media;
+		
+		Scanner leia = new Scanner(System.in);
+		
+		System.out.println("\nEntre com um número: ");
+		numeros = leia.nextInt();	
+		do {		
+			if(numeros == 0) {
+				System.out.println("\nSair do loop!!!");
+			}else {
+				
+				if(numeros % 3 == 0) {
+					somaMult3 += numeros;
+					contMult3++;
+				}
+				System.out.println("\nEntre com um número: ");
+				numeros = leia.nextInt();
+			}
+			
+		}while(numeros != 0);
+		
+		if(contMult3 == 0) {
+			System.out.println("\nNão podemos dividir por ZERO!!!");
+		}else {
+			
+			media = somaMult3 / contMult3;
+			System.out.printf("\nMédia dos números multiplos de 3: %.2f",media);
+		 }
+	 }
+}
+
+
+
+
